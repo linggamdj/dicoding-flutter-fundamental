89f808417941b67a9f06af2691990c6c86886729
Lingga <linggaadiatmdj@gmail.com>
2022-10-21 13:20:46 +0700

    refactor: provider

--- news_app/lib/ui/article_list_page.dart
+++ news_app/lib/ui/article_list_page.dart
@@ -1,54 +1,49 @@
 import 'package:news_app/common/style.dart';
 import 'package:news_app/data/models/article_model.dart';
+import 'package:news_app/provider/news_provider.dart';
 import 'article_detail_page.dart';
 import 'package:news_app/widgets/platform_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/data/api/api_service.dart';
 import 'package:news_app/widgets/card_article.dart';
+import 'package:provider/provider.dart';
 
-class ArticleListPage extends StatefulWidget {
+class ArticleListPage extends StatelessWidget {
   const ArticleListPage({Key? key}) : super(key: key);
 
-  @override
-  State<ArticleListPage> createState() => _ArticleListPageState();
-}
-
-class _ArticleListPageState extends State<ArticleListPage> {
-  late Future<ArticlesResult> _article;
-
-  @override
-  void initState() {
-    super.initState();
-    _article = ApiService().topHeadlines();
-  }
-
-  Widget _buildList(BuildContext context) {
-    return FutureBuilder(
-      future: _article,
-      builder: (context, AsyncSnapshot<ArticlesResult> snapshot) {
-        var state = snapshot.connectionState;
-        if (state != ConnectionState.done) {
+  Widget _buildList() {
+    return Consumer<NewsProvider>(
+      builder: (context, state, _) {
+        if (state.state == ResultState.loading) {
           return const Center(child: CircularProgressIndicator());
+        } else if (state.state == ResultState.hasData) {
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: state.result.articles.length,
+            itemBuilder: (context, index) {
+              var article = state.result.articles[index];
+              return CardArticle(article: article);
+            },
+          );
+        } else if (state.state == ResultState.noData) {
+          return Center(
+            child: Material(
+              child: Text(state.message),
+            ),
+          );
+        } else if (state.state == ResultState.error) {
+          return Center(
+            child: Material(
+              child: Text(state.message),
+            ),
+          );
         } else {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              shrinkWrap: true,
-              itemCount: snapshot.data?.articles.length,
-              itemBuilder: (context, index) {
-                var article = snapshot.data?.articles[index];
-                return CardArticle(article: article!);
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Material(
-                child: Text(snapshot.error.toString()),
-              ),
-            );
-          } else {
-            return const Material(child: Text(''));
-          }
+          return const Center(
+            child: Material(
+              child: Text(''),
+            ),
+          );
         }
       },
     );
@@ -59,7 +54,7 @@ class _ArticleListPageState extends State<ArticleListPage> {
       appBar: AppBar(
         title: const Text('News App'),
       ),
-      body: _buildList(context),
+      body: _buildList(),
     );
   }
 
@@ -69,7 +64,7 @@ class _ArticleListPageState extends State<ArticleListPage> {
         middle: Text('News App'),
         transitionBetweenRoutes: false,
       ),
-      child: _buildList(context),
+      child: _buildList(),
     );
   }
 
